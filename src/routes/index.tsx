613ba7983b05fbd43c1e5c817c36dd0000ed1ce9
Aleh Khantsevich <aleh.khantsevich@gmail.com>
2024-11-11 14:25:32 +0100

    display row

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,44 +4,63 @@ import { Button } from "~/components/core/Button/Button";
 import { ToggleSwitch } from "~/components/core/ToggleSwitch/ToggleSwitch";
 
 export default function Home() {
-  const [switchState, setSwitchState] = createSignal(false);
+	const [switchState, setSwitchState] = createSignal(false);
 	return (
 		<main
 			style={{
 				display: "flex",
 				"flex-direction": "column",
 				gap: "1rem",
-				width: "200px",
+				color: "white",
 			}}
 		>
 			<Title>Components</Title>
-			<Button variant={switchState() ? "accent" : "standard"}>Standard</Button>
-			<Button variant={"accent"}>Accent</Button>
-			<Button variant={"hyperlink"}>Click me</Button>
-			<Button variant={"standard"} disabled>
-				Standard Diasbled
-			</Button>
-			<Button variant={"accent"} disabled>
-				Accent Disabled
-			</Button>
-			<Button variant={"hyperlink"} disabled>
-				HyperLink disabled
-			</Button>
-      <ToggleSwitch label="Default" />
-      <ToggleSwitch label="Default Disabled"  disabled/>
-      <ToggleSwitch defaultChecked label="Checked disabled" disabled/>
-			<ToggleSwitch defaultChecked label="Default checked" />
-      <ToggleSwitch description="Description" label="Default description" />
-			<ToggleSwitch
-				checked={switchState()}
-				onChange={(checked) => setSwitchState(checked)}
-        label="Binded" 
-			/>
-			<ToggleSwitch
-				checked={switchState()}
-				onChange={(checked) => setSwitchState(checked)}
-        label="Binded two" 
-			/>
+			Buttons
+			<div
+				style={{
+					display: "flex",
+					"flex-direction": "row",
+					gap: "1rem",
+				}}
+			>
+				<Button variant={switchState() ? "accent" : "standard"}>
+					Standard
+				</Button>
+				<Button variant={"accent"}>Accent</Button>
+				<Button variant={"hyperlink"}>Click me</Button>
+				<Button variant={"standard"} disabled>
+					Standard Diasbled
+				</Button>
+				<Button variant={"accent"} disabled>
+					Accent Disabled
+				</Button>
+				<Button variant={"hyperlink"} disabled>
+					HyperLink disabled
+				</Button>
+			</div>
+			ToggleSwitch
+			<div
+				style={{
+					display: "flex",
+					"flex-direction": "row",
+				}}
+			>
+				<ToggleSwitch label="Default" />
+				<ToggleSwitch label="Default Disabled" disabled />
+				<ToggleSwitch defaultChecked label="Checked disabled" disabled />
+				<ToggleSwitch defaultChecked label="Default checked" />
+				<ToggleSwitch description="Description" label="Default description" />
+				<ToggleSwitch
+					checked={switchState()}
+					onChange={(checked) => setSwitchState(checked)}
+					label="Binded"
+				/>
+				<ToggleSwitch
+					checked={switchState()}
+					onChange={(checked) => setSwitchState(checked)}
+					label="Binded two"
+				/>
+			</div>
 		</main>
 	);
 }
