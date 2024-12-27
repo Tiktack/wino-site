c390940c7546df01a2cbbd1f46b43d49b040503c
Aleh Khantsevich <aleh.khantsevich@gmail.com>
2024-12-27 21:57:59 +0100

    Added basic footer

--- src/components/layouts/MainLayout/Footer.tsx
+++ src/components/layouts/MainLayout/Footer.tsx
@@ -1,9 +1,67 @@
+import { A } from "@solidjs/router";
 import { format } from "date-fns/format";
+import { Button } from "~/components/core/Button/Button";
+import { TextBlock } from "~/components/core/TextBlock/TextBlock";
 
 export const Footer = () => {
 	return (
-		<footer class="bg-gray-800 text-white p-4 text-center">
-			© {format(new Date(), "yyyy")} Wino Mail
+		<footer class="bg-[var(--solid-background-base)] p-4 flex justify-around">
+			<div class="flex flex-col gap-y-2">
+				<div class="flex items-center gap-x-2">
+					<img src="/logo.png" alt="Wino Mail Logo" class="w-8" />
+					<TextBlock variant="subtitle">Wino Mail</TextBlock>
+				</div>
+				<TextBlock
+					style={{
+						color: "var(--text-tertiary)",
+					}}
+				>
+					Copyright (c) {format(new Date(), "yyyy")} Wino
+				</TextBlock>
+			</div>
+			<div class="flex flex-col gap-y-1">
+				<TextBlock
+					style={{
+						padding: "0.6rem",
+						color: "var(--text-tertiary)",
+					}}
+				>
+					Contribute
+				</TextBlock>
+				<Button
+					variant="hyperlink"
+					as="a"
+					href="https://github.com/bkaankose/Wino-Mail/issues/new/choose"
+					target="_blank"
+				>
+					GitHub Issue
+				</Button>
+				<Button
+					variant="hyperlink"
+					as="a"
+					href="https://github.com/bkaankose/Wino-Mail/blob/main/CONTRIBUTING.md"
+					target="_blank"
+				>
+					Contribution Guideline
+				</Button>
+			</div>
+			<div class="flex flex-col gap-y-1">
+				<TextBlock
+					style={{
+						padding: "0.6rem",
+						color: "var(--text-tertiary)",
+					}}
+				>
+					Support
+				</TextBlock>
+				<Button
+					variant="hyperlink"
+					as={A}
+					href="/Privacy"
+				>
+					Privacy
+				</Button>
+			</div>
 		</footer>
 	);
 };
