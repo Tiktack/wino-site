1d4629fe25f4df53b000594b495522901cefd954
Aleh Khantsevich <aleh.khantsevich@gmail.com>
2024-12-28 14:44:16 +0100

    feat: update navbar styles

--- src/components/layouts/MainLayout/Navbar.tsx
+++ src/components/layouts/MainLayout/Navbar.tsx
@@ -1,28 +1,35 @@
 import { For, type JSX } from 'solid-js';
-import { useLocation } from '@solidjs/router';
+import { A, useLocation } from '@solidjs/router';
 import styles from "./Navbar.module.css"
 import { SiDiscord, SiGithub } from "solid-icons/si";
 import { TextBlock } from '~/components/core/TextBlock/TextBlock';
 import { IconButton } from '~/components/core/IconButton/IconButton';
+import { FiHome } from 'solid-icons/fi'
+import { TbNews } from 'solid-icons/tb'
+import { IoDocumentTextOutline } from 'solid-icons/io'
+import type { IconTypes } from 'solid-icons';
 
 interface Item {
   href: string;
   name: string;
-  icon?: string;
+  Icon?: IconTypes;
 }
 
 const NAVBAR_ITEMS: Item[] = [
   {
     href: "/",
     name: "Home",
+    Icon: FiHome
   },
   {
     href: "/blog",
     name: "Blog",
+    Icon: TbNews,
   },
   {
     href: "/docs/overview",
     name: "Documentation",
+    Icon: IoDocumentTextOutline,
   },
   {
     href: "/components/buttons",
@@ -55,22 +62,24 @@ export const Navbar = () => {
         <nav>
           <For each={NAVBAR_ITEMS}>
             {(item) => (
-              <a
+              <A
                 href={item.href}
-                class={isSelected(item.href) ? 'selected' : ''}
+                // class={isSelected(item.href) ? 'selected' : ''}
+                activeClass='bg-accent-tertiary'
+                end
               >
-                {item.icon && <div innerHTML={item.icon} />}
+                {item.Icon && <item.Icon class='mr-1' />}
                 <TextBlock>{item.name}</TextBlock>
-              </a>
+              </A>
             )}
           </For>
         </nav>
         <div class={styles.buttons}>
           <div style={{ display: "flex", "flex-direction": "row" }}>
-            <IconButton variant="standard">
+            <IconButton as={'a'} variant="standard" href='https://discord.com/invite/windows-apps-hub-714581497222398064' target='_blank'>
               <SiDiscord />
             </IconButton>
-            <IconButton variant="standard" disabled>
+            <IconButton as={'a'} variant="standard" href='https://github.com/bkaankose/Wino-Mail/' target='_blank'>
               <SiGithub />
             </IconButton>
           </div>
