dd7629b050bad007b0ac8da52ef0e300e611568a
Tiktack <aleh.khantsevich@gmail.com>
2025-01-06 12:34:03 +0000

    Deploying to gh-pages from @ Tiktack/wino-site@3182548d33a7f2cb3953b34703b5977760bb17d0 🚀

--- _build/assets/E53DB7BS-BXT6dB8m.js
+++
@@ -1,1 +0,0 @@
-import{b as c}from"./JNCCF6MP-C0sLKbp_.js";import{b as l}from"./5WXHJDCZ-DDWJ21Wx.js";function f(e={}){const[o,n]=c({value:()=>l(e.open),defaultValue:()=>!!l(e.defaultOpen),onChange:s=>e.onOpenChange?.(s)}),t=()=>{n(!0)},a=()=>{n(!1)};return{isOpen:o,setIsOpen:n,open:t,close:a,toggle:()=>{o()?a():t()}}}export{f as c};
---
+++ _build/assets/install-CckdAAGr.js
@@ -0,0 +1,1 @@
+import{i as n,p as r,k as i,b as s,t as o}from"./web-CwqXaN1j.js";import{T as m}from"./index-BpkHBRql.js";var p=o("<main><!$><!/>Install");function f(){return(()=>{var t=n(p),a=t.firstChild,[e,l]=r(a.nextSibling);return e.nextSibling,i(t,s(m,{children:"Wino Mail | Docs | Install"}),e,l),t})()}export{f as default};
