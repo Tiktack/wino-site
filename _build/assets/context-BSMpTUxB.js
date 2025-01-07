128a2d44bb976d988bb2803eab16d19c9336e73b
Tiktack <aleh.khantsevich@gmail.com>
2025-01-07 23:07:05 +0000

    Deploying to gh-pages from @ Tiktack/wino-site@cd228a79a9108d064b737fe133e8113679a0bd75 🚀

---
+++ _build/assets/context-BSMpTUxB.js
@@ -0,0 +1,1 @@
+import{B as t}from"./web-DJ7BZSlE.js";const o=e=>new Proxy({},{get:()=>{throw new Error(e)}}),n=t(o("ThemeContext is used outside of its Provider"));export{n as T};
---
+++ _build/assets/install-CTR6dbdL.js
@@ -0,0 +1,1 @@
+import{i as n,n as r,k as i,b as s,t as o}from"./web-DJ7BZSlE.js";import{T as m}from"./index-BtZKIbFE.js";var p=o("<main><!$><!/>Install");function f(){return(()=>{var t=n(p),a=t.firstChild,[e,l]=r(a.nextSibling);return e.nextSibling,i(t,s(m,{children:"Wino Mail | Docs | Install"}),e,l),t})()}export{f as default};
