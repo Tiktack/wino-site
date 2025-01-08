f9a5012ca4db3b71a661af4d42cf9454dfd5236a
Aleh Khantsevich <aleh.khantsevich@gmail.com>
2025-01-09 00:59:07 +0100

    feat: redesign 404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,19 +1,36 @@
 import { Title } from '@solidjs/meta';
+import { A } from '@solidjs/router';
 import { HttpStatusCode } from '@solidjs/start';
+import * as stylex from '@stylexjs/stylex';
+import { Button } from '~/components/core/Button/Button';
+import { TextBlock } from '~/components/core/TextBlock/TextBlock';
 
 export default function NotFound() {
 	return (
-		<main>
-			<Title>Not Found</Title>
+		<main {...stylex.attrs(styles.main)}>
+			<Title>Wino | Not Found</Title>
 			<HttpStatusCode code={404} />
-			<h1>Page Not Found</h1>
-			<p>
-				Visit{' '}
-				<a href="https://start.solidjs.com" target="_blank" rel="noreferrer">
-					start.solidjs.com
-				</a>{' '}
-				to learn how to build SolidStart apps.
-			</p>
+			<TextBlock variant="title">
+				The page you are looking for does not exist.
+			</TextBlock>
+			<TextBlock variant="body">
+				But hey, our home page is full of interesting stuff! Why don't you check
+				it out instead? 🏡✨
+			</TextBlock>
+			<Button variant="accent" as={A} href="/">
+				Go home
+			</Button>
 		</main>
 	);
 }
+
+const styles = stylex.create({
+	main: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+		height: '100vh',
+		gap: '1rem',
+	},
+});
