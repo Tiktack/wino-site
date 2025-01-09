4747a8c27774d798e176c95f27b7c7055a507c93
Aleh Khantsevich <aleh.khantsevich@gmail.com>
2025-01-09 18:14:36 +0100

    revert: to gihubPages

--- app.config.ts
+++ app.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
 	ssr: true,
 	server: {
 		baseURL: process.env.BASE_PATH,
-		preset: 'azure_swa',
+		preset: 'githubPages',
 		static: true,
 		prerender: {
 			crawlLinks: true,
